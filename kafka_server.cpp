60cd2a1ebc7c55a0df6b418569f87ddc006fed12
Noah Phonsavath <nphonsavath@gmail.com>
2025-07-25 16:29:21 -0600

    Added single client connection to server.

--- kafka_server.cpp
+++ kafka_server.cpp
@@ -1,34 +1,58 @@
-#include<iostream>
+#include <iostream>
+#include <unistd.h>
+
 #include <sys/socket.h>
-
-
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main() {
-    //Create TCP socket using IPv4. 
-    int serverFD = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverFD  == -1) {
-        std::cerr << "Error creating server socket. " << std::endl;
-        return 1;
-    }
+	//Create TCP socket using IPv4. 
+	int serverFD = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverFD  == -1) {
+		std::cerr << "Error creating server socket." << std::endl;
+		return 1;
+	}
 
-    struct sockadder_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(9092); //default port # for kafka
-    server.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(9092); //default port # for kafka
+	server.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(serverFD, (struct sockaddr*) &server), sizeof(server)) == -1) {
-        close(serverFD);
-        std::cerr << "Error binding server socket to 9092" << std::endl;
-        return 1;
-    }
+	if (bind(serverFD, (struct sockaddr*) &server, sizeof(server)) == -1) {
+		close(serverFD);
+		std::cerr << "Error binding server socket to 9092." << std::endl;
+		return 1;
+	}
 
-    if (getsockname(serverFD, (struct sockaddr* &server), sizeof(server)) == -1-) {
-        close(serverFD);
-        std::cerr << "Error retrieving socket address" << std::endl;
-    }
+	socklen_t serverLength = sizeof(server);
+	if (getsockname(serverFD, (struct sockaddr*) &server, &serverLength) == -1) {
+		close(serverFD);
+		std::cerr << "Error retrieving socket address." << std::endl;
+		return -1;
+	}
 
-    std::cout << "IP Adress: " << server.sin_addr << ", Port #: " << server.sin_port << std::endl;
-    
-    int maxConnectionRequests = 5;
-    
+	char ipAsString[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(server.sin_addr), ipAsString, INET_ADDRSTRLEN);
+	std::cout << "IP Adress: " << ipAsString << ", Port #: " << ntohs(server.sin_port) << std::endl;
+
+	int maxConnectionRequests = 5;
+	if (listen(serverFD, maxConnectionRequests) == -1) {
+		close(serverFD);
+		std::cerr << "Error listening for connections." << std::endl;
+		return 1;	
+	}
+
+	struct sockaddr_in client;
+	socklen_t clientLength = sizeof(client);
+	int clientFD = accept(serverFD, (struct sockaddr*) &client, &clientLength); 
+	if (clientFD == -1) {
+		close(serverFD);
+		std::cerr << "Error connecting client with server." << std::endl;
+		return 1;
+	}
+
+	std::cout << "Client connected\n";
+	close(clientFD);
+	close(serverFD);
+	return 0;
 }
